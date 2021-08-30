2a895846d404a10b46ce2c367018981a5deb2e0c
Mr-lixh <a601287411@163.com>
2021-08-30 23:49:21 +0800

    http优雅终止

--- builtin_package_demo/http_test.go
+++ builtin_package_demo/http_test.go
@@ -1,9 +1,13 @@
 package builtin_package_demo
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"testing"
 	"time"
 )
@@ -36,4 +40,45 @@ func TestMyHandler(t *testing.T) {
 		MaxHeaderBytes: 1 << 20,
 	}
 	log.Fatalln(s.ListenAndServe())
+}
+
+// TestGraceShutdown 优雅终止 http 服务
+func TestGraceShutdown(t *testing.T) {
+	h := &MyHandler{
+		foo: "hello world!",
+	}
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        h,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	done := make(chan bool)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		log.Println("Server is shutting down...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		if err := s.Shutdown(ctx); err != nil {
+			log.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		}
+		close(done)
+	}()
+
+	log.Println("Server is ready to handle requests at : 8080")
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Could not listen on 8080: %v\n", err)
+	}
+
+	<-done
+	log.Println("Server stopped")
 }
