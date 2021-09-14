b81e705965b956152502b4e3eb80dd2ce81bc5f7
xhli18 <xhli18@iflytek.com>
2021-09-14 22:01:28 +0800

    docker sdk

--- go.mod
+++ go.mod
@@ -3,18 +3,22 @@ module github.com/Mr-lixh/go-demo
 go 1.15
 
 require (
+	github.com/containerd/containerd v1.5.5 // indirect
+	github.com/docker/docker v20.10.8+incompatible
+	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/googleapis/gnostic v0.5.5 // indirect
-	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/pelletier/go-toml v1.9.3
-	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.11.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.8.1
-	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0
-	k8s.io/api v0.0.0-20181128191700-6db15a15d2d3
-	k8s.io/apimachinery v0.0.0-20181128191346-49ce2735e507
-	k8s.io/client-go v0.0.0-20190115164855-701b91367003
+	k8s.io/api v0.20.6
+	k8s.io/apimachinery v0.20.6
+	k8s.io/client-go v0.20.6
 )
---
+++ thirdparty_package_demo/docker_demo/docker_test.go
@@ -0,0 +1,157 @@
+package docker_demo
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	"io"
+	"os"
+	"testing"
+)
+
+// 拉取镜像并启动一个容器
+func TestRunContainer(t *testing.T) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// 拉取镜像
+	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer reader.Close()
+	io.Copy(os.Stdout, reader)
+
+	// 创建容器
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"echo", "hello world"},
+		Tty:   false,
+	}, nil, nil, nil, "")
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// 启动容器
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// 获取容器状态
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+	case <-statusCh:
+	}
+
+	// 获取容器标准输出日志
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+}
+
+// 查询本地所有的容器
+func TestListContainers(t *testing.T) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	for _, c := range containers {
+		fmt.Println(c.ID)
+	}
+}
+
+// 查询本地所有的镜像
+func TestListImages(t *testing.T) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	images, err := cli.ImageList(ctx, types.ImageListOptions{})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	for _, i := range images {
+		fmt.Println(i.ID)
+	}
+}
+
+// 拉取镜像
+func TestPullImage(t *testing.T) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	authConfig := types.AuthConfig{
+		Username: "admin",
+		Password: "Harbor12345",
+	}
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+
+	out, err := cli.ImagePull(ctx, "alpine", types.ImagePullOptions{RegistryAuth: authStr})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	defer out.Close()
+
+	io.Copy(os.Stdout, out)
+}
+
+// 获取容器进程信息
+func TestTopContainer(t *testing.T) {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	body, err := cli.ContainerTop(ctx, "container-id", []string{})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	// 获取容器所有 PID
+	var PIDs []string
+	var index int
+	for i, t := range body.Titles {
+		if t == "PID" {
+			index = i
+			break
+		}
+	}
+	for _, proc := range body.Processes {
+		PIDs = append(PIDs, proc[index])
+	}
+
+	fmt.Println(PIDs)
+}
